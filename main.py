00c0aef2e9e1bad078649fb2994c0fcebdfa0311
tilmantnetious <tilman.schlichting@uni-jena.de>
2021-10-27 12:28:09 +0200

    -- created simulation structure

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ Corona Spread Prediction
 
 """
 from ruleset import ruleset
+from simulation import sim
 
 class Main:
     def __init__(self):
@@ -20,6 +21,9 @@ runtime = Main()
 
 # Create a ruleset with Paramenters from JSON
 runtime.ruleset = ruleset.Ruleset(runtime.config).getRuleset()
+
+#Create a Simulation with given Rulesets
+runtime.sim = sim.Sim(runtime.ruleset)
 
 #Debugpoint
 print("hold")
